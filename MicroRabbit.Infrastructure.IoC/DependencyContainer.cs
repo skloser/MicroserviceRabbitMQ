220e527d360af742272b36394d5ab3f1545a101f
Крум Багашев <krum.bagashev@gmail.com>
2020-11-01 23:58:33 +0200

    Added IoC container

---
+++ MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace MicroRabbit.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
