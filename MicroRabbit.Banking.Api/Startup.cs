ceec633b126565162b928131ff72cbc33662168d
Крум Багашев <krum.bagashev@gmail.com>
2020-11-08 12:23:42 +0200

    Added IoC container

--- MicroRabbit.Banking.Api/Startup.cs
+++ MicroRabbit.Banking.Api/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MicroRabbit.Infrastructure.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +26,13 @@ namespace MicroRabbit.Banking.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            RegisterServices(services);
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            //DependencyContainer.RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
+++ MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
@@ -1,4 +1,9 @@
-﻿using MicroRabbit.Domain.Core.Bus;
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infrastructure.Bus;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -9,7 +14,16 @@ namespace MicroRabbit.Infrastructure.IoC
     {
         public static void RegisterServices(IServiceCollection services)
         {
+
+            //Domain bus
             services.AddTransient<IEventBus, RabbitMQBus>();
+
+            //Application services
+            services.AddTransient<IAccountService, AccountService>();
+
+            //Data 
+            services.AddTransient<IAccountRepository, AccountRepository>();
+            services.AddTransient<BankingDbContext>();
         }
     }
 }
