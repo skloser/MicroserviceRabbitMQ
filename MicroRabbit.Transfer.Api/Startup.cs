e21ebf0681eaea64dfa3c30a7e35518da7b4b6a5
Крум Багашев <krum.bagashev@gmail.com>
2021-01-02 19:43:25 +0200

    Added db context and migrations

--- MicroRabbit.Transfer.Api/Startup.cs
+++ MicroRabbit.Transfer.Api/Startup.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using MediatR;
+using MicroRabbit.Infrastructure.IoC;
+using MicroRabbit.Transfer.Data.Context;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,7 +30,24 @@ namespace MicroRabbit.Transfer.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TransferDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("TransferDbConnection"));
+
+            });
+
+
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Transfer microservice", Version = "v1" });
+            });
+
+            services.AddMediatR(typeof(Startup));
+
+            DependencyContainer.RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +61,12 @@ namespace MicroRabbit.Transfer.Api
             {
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking microservice v1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
---
+++ MicroRabbit.Transfer.Data/Context/TransferDbContext.cs
@@ -0,0 +1,35 @@
+﻿using MicroRabbit.Transfer.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MicroRabbit.Transfer.Data.Context
+{
+    public class TransferDbContext : DbContext
+    {
+        public TransferDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TransferLog> TransferLogs { get; set; }
+    }
+
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<TransferDbContext>
+    {
+        public TransferDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<TransferDbContext>();
+            var connectionString = configuration.GetConnectionString("TransferDbConnection");
+            builder.UseSqlServer(connectionString);
+            return new TransferDbContext(builder.Options);
+        }
+    }
+}
